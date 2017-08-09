fbb414d6092962e55ef76351f6a899506b0bf892
Surbma <peter@surbma.hu>
2017-08-09 11:35:36 +0200

    1.1.1
    - Changed the way, it loads the parent theme's CSS file.

--- functions.php
+++ functions.php
@@ -1,12 +1,7 @@
 <?php
 
-// Replace default stylesheet
-function extra_plus_default_stylesheet() {
-	return get_template_directory_uri() . '/style.css';
-}
-add_filter( 'stylesheet_uri', 'extra_plus_default_stylesheet', 10, 2 );
-
 function extra_plus_enqueues() {
+	wp_enqueue_style( 'extra', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'extra-plus', get_stylesheet_directory_uri() . '/css/extra-plus.css' );
 }
 add_action( 'wp_enqueue_scripts', 'extra_plus_enqueues', 20 );
@@ -17,7 +12,7 @@ function extra_plus_widgets_init() {
 		'name' => 'Extra 404',
 		'id' => 'extra-404',
 		'before_widget' => '<div id="%1$s" class="et_404_widget %2$s">',
-		'after_widget' => '</div> <!-- end .et_404_widget -->',
+		'after_widget' => '</div>',
 		'before_title' => '<h4 class="widgettitle">',
 		'after_title' => '</h4>'
 	) );
