52134edbd9a3970c379fc881712251b43063897f
Surbma <peter@surbma.hu>
2017-09-02 23:45:57 +0200

    1.1.2
    - Updated index.php file with the Extra theme's modifications.

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
 		<div id="content-area" class="<?php extra_sidebar_class(); ?> clearfix">
 			<div class="et_pb_extra_column_main">
 				<?php if ( is_search() ) { ?>
-					<h1><?php esc_html_e( sprintf( __( 'Search Results for: %s', 'extra' ), get_search_query() ) ); ?></h1>
+					<h1><?php printf( esc_html__( 'Search Results for: %s', 'extra' ), get_search_query() ); ?></h1>
 				<?php } else if ( is_archive() ) { ?>
 					<h1><?php the_archive_title(); ?></h1>
 					<div class="archive-description"><?php echo term_description(); ?></div>
 				<?php } ?>
 
-				<?php if ( is_category() || is_tag() ) { ?>
+				<?php if ( is_extra_tax_layout() ) { ?>
 					<?php extra_tax_layout(); ?>
 				<?php } else { ?>
 					<?php require locate_template( 'index-content.php' ); ?>
